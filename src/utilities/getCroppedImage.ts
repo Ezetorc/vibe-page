6e7db35f6422a0c2f87743ac2c6189024193de06
Ezetorc <ezetorc@gmail.com>
2025-03-23 23:12:27 -0300

    Minor patch

--- src/utilities/getCroppedImage.ts
+++ src/utilities/getCroppedImage.ts
@@ -10,6 +10,7 @@ export async function getCroppedImage (
   const image = await getImage(imageSrc)
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
+  
 
   if (!ctx) return null
 
